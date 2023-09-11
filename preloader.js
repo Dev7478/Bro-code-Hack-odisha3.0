e0878708e678ebff4d8f3edbf7f939a56a154d33
Arnab Sarkar <110808978+arnab236@users.noreply.github.com>
2023-09-11 12:46:36 +0530

    Add files via upload

---
+++ preloader.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function(){
+    this.document.getElementById("preloader").style.display= "none";
+});
+
+window.addEventListener("load", function(){
+    this.document.getElementById("loader").style.display= "none";
+});
